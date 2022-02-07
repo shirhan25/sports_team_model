2916f472857b74c514e19e5ca7c87445c38f65d9
Shirhan <shirhan25@hotmail.com>
2022-02-07 15:31:24 +0000

    methods added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,14 +9,44 @@ public class Main {
 
         Player player = new Player (" Fin Rusell" , "Fly" , 10, 10 );
 
-
+// creating a manager object
     Manager manager = new Manager( "Steve");
+ // creating a team object with properties like name and manager
         Team team = new Team("Scotland", manager);
+        //creating the team service object
     TeamService teamService = new TeamService();
-            //creating the team service object
 
-        int space = teamService.countEmptySpacesOnTeam(team);
-        System.out.println(space);
+//counting empty spaces on the team
+        int spaceBefore = teamService.countEmptySpacesOnTeam(team);
+        System.out.println(spaceBefore);
+        //try this - adding player to a team
+        try {
+            teamService.addPlayerToTeam(team,player);
+            teamService.addPlayerToTeam(team,player);
+            teamService.addPlayerToTeam(team,player);
+            teamService.addPlayerToTeam(team,player);
+            teamService.addPlayerToTeam(team,player);
+            teamService.addPlayerToTeam(team,player);
+            teamService.addPlayerToTeam(team,player);
+            teamService.addPlayerToTeam(team,player);
+            teamService.addPlayerToTeam(team,player);
+            teamService.addPlayerToTeam(team,player);
+            teamService.addPlayerToTeam(team,player);
+            teamService.addPlayerToTeam(team,player);
+            teamService.addPlayerToTeam(team,player);
+            teamService.addPlayerToTeam(team,player);
+            teamService.addPlayerToTeam(team,player);
+            teamService.addPlayerToTeam(team,player);
+            
+           // catch it when its full and throw an exception
+        } catch (Exception e)  {
+            System.out.println("team is full");
+        }
+
+
+        //checking spaces you have left adding a player
+        int spacesAfter = teamService.countEmptySpacesOnTeam(team);
+        System.out.println(spacesAfter);
     }
 
 }
--- src/main/java/team/TeamService.java
+++ src/main/java/team/TeamService.java
@@ -7,7 +7,7 @@ public class TeamService {
 
 //default constructor - you dont need it here
 
-    public TeamService(){
+    public TeamService() {
 
 
     }
@@ -15,15 +15,15 @@ public class TeamService {
 //return an int
 // parameter: Team - information we can access within team
 
-    public int countEmptySpacesOnTeam(Team team){
-       // create a placeholder variable to store the count, starts at 0
+    public int countEmptySpacesOnTeam(Team team) {
+        // create a placeholder variable to store the count, starts at 0
         int count = 0;
 
         //looking through player[] for nulls via loop and count null
-        for (Player player: team.getPlayers()) {
-          // getplayers gets the array for players
-       //if statement, if it equals to null add 1 to the count. == compared the ref, checking for absence of something use ==
-            if(player == null) {
+        for (Player player : team.getPlayers()) {
+            // getplayers gets the array for players
+            //if statement, if it equals to null add 1 to the count. == compared the ref, checking for absence of something use ==
+            if (player == null) {
 // how to add one to the count
                 count++;
 // no else statement as we don't do anything if there's a player
@@ -48,18 +48,33 @@ public class TeamService {
  */
 
 // adding player to team / no need to return & just need to do
-
-  //  public void addPlayerToTeam(Team team, Player player){
-     //  int spaces = countEmptySpacesOnTeam(team);
+// telling the compiler something could throw an exception
+    public void addPlayerToTeam(Team team, Player player) throws Exception {
+        //checking spaces on the team
+        int spaces = countEmptySpacesOnTeam(team);
         //if there is, add the player
-     //   if(spaces >0){
-       // get players array from team
-       // add player to first avaliable empty space
-       //loop through players array
-       // if we find an empty space, add the player then break
+        if (spaces > 0) {
+            // get players array from team,create a variable to store
+            Player[] players = team.getPlayers();
+            // add player to first avaliable empty space
+            //loop through players array
+            for (int i = 0; i < players.length; i++) {
+                //if we find an empty space, add the player the beak
 
+                if (players[i] == null) {
+                    //adding the player
+                    players[i] = player;
+                    break;
+                }
+            }
+        }
+        //outside the if statement
+        else {
+            throw new Exception();
         }
 
+}
+}
 
 
 
